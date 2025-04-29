ee4cc06655c4bc5861c5eeab6f5ba03e119dff8d
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2025-04-29 11:17:09 +0530

    revamped use env variables

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,6 @@
 import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
+import {env} from "./src/env";
 
 config({ path: ".env" });
 
@@ -8,7 +9,7 @@ export default defineConfig({
   out: "./src/db/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: env.DATABASE_URL!,
   },
 });
 
--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,6 +1,7 @@
 import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/neon-http";
+import { env } from "../env";
 
 config({ path: ".env" });
 
-export const db = drizzle(process.env.DATABASE_URL!);
+export const db = drizzle(env.DATABASE_URL!);
